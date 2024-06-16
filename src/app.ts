b3250b8518ca263478fd35858881a495539ecadc
Zach Baker <zach@zachbaker.dev>
2024-06-16 12:55:19 -0400

    feat: add server router to app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import sequelize, { sequelizeLogger } from "./database/sequelize";
+import ServerRouter from "./routers/ServerRouter";
 import UserRouter from "./routers/UserRouter";
 import logger from "./util/logger";
 
@@ -14,6 +15,7 @@ app.use(express.json());
 
 // Routers
 app.use("/user", UserRouter);
+app.use("/server", ServerRouter);
 
 // Sync database and start server
 sequelize
